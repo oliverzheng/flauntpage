3d8b9e5167d64e15f186500c6c43ea60ffb71c9d
Oliver Zheng <gitcommits@oliverzheng.com>
2024-08-31 17:48:12 -0700

    Add ability to upload dummy file

--- dev-server/server.ts
+++ dev-server/server.ts
@@ -1,29 +1,54 @@
 import fs from 'node:fs/promises';
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
 import {createServer as createViteServer} from 'vite';
 
 const port = process.env.PORT || 5173;
+const STORED_FILES_DIR = 'stored-files';
+const TMP_FILES_DIR = 'tmp-stored-files';
 
 const app = express();
 
 const vite = await createViteServer({
-  server: {middlewareMode: true},
+  server: {
+    middlewareMode: true,
+    watch: {
+      ignored: `**/${STORED_FILES_DIR}/**`,
+    },
+  },
   appType: 'custom',
 });
 app.use(vite.middlewares);
 
-app.use('*', async (req, res) => {
-  try {
-    const url = req.originalUrl.replace('/', '');
+const upload = multer({dest: TMP_FILES_DIR});
 
-    // Always read fresh template in development
-    const template = await fs.readFile('./index.html', 'utf-8');
-    const html = await vite.transformIndexHtml(url, template);
+app.post('/upload', upload.single('file'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const targetPath = path.join(STORED_FILES_DIR, req.file.originalname);
+  await fs.rename(req.file.path, targetPath);
+
+  return res.send('Uploaded');
+});
+
+app.get('*.html', async (req, res) => {
+  const url = req.originalUrl;
+
+  try {
+    const file = await fs.readFile(STORED_FILES_DIR + url, 'utf-8');
+    const html = await vite.transformIndexHtml(url, file);
 
     res.status(200).set({'Content-Type': 'text/html'}).send(html);
   } catch (e) {
     if (!(e instanceof Error)) {
       throw e;
+    }
+
+    if ('code' in e && e.code === 'ENOENT') {
+      return res.status(404).end();
     }
 
     vite?.ssrFixStacktrace(e);
--- src/entry-client.ts
+++ src/entry-client.ts
@@ -22,3 +22,20 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `;
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+
+const uploadButton =
+  document.querySelector<HTMLButtonElement>('#upload-button')!;
+uploadButton.addEventListener('click', async () => {
+  const formData = new FormData();
+  const file = new File(
+    ['Hello, world!'],
+    'my-path/to/here/my-custom-file-name.html',
+    {type: 'text/html'},
+  );
+  formData.append('file', file);
+  const response = await fetch('/upload', {
+    method: 'POST',
+    body: formData,
+  });
+  console.log({response});
+});
