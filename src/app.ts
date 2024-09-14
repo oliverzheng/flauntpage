a2edfa9b799b7d24004170421ebeae025a3a4076
Oliver Zheng <gitcommits@oliverzheng.com>
2024-09-13 21:22:46 -0700

    Add new page

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ export function initApp(chromeElement: Element) {
   );
   invariant(enableEditButton, 'Missing enable-edit-button');
 
+  const addPageButton =
+    chromeElement.querySelector<HTMLButtonElement>('.add-page-button');
+  invariant(addPageButton, 'Missing add-page-button');
+
   const cancelEditButton = chromeElement.querySelector<HTMLButtonElement>(
     '.cancel-edit-button',
   );
@@ -21,6 +25,10 @@ export function initApp(chromeElement: Element) {
     turnOnEditMode();
   });
 
+  addPageButton.addEventListener('click', async () => {
+    await onAddPageClick();
+  });
+
   saveButton.addEventListener('click', async () => {
     const newContent = await getEditorContentHtml();
     await uploadFile('index.html', newContent);
@@ -30,4 +38,24 @@ export function initApp(chromeElement: Element) {
   cancelEditButton.addEventListener('click', async () => {
     turnOffEditMode();
   });
+}
+
+async function onAddPageClick() {
+  let url = prompt('What url should the page have?', '/somepage.html');
+
+  while (true) {
+    if (url == null) {
+      return;
+    } else if (!url.startsWith('/')) {
+      url = prompt('Url must start with /', '/somepage.html');
+    } else if (!url.endsWith('.html')) {
+      url = prompt('Url must end with .html', '/somepage.html');
+    } else {
+      break;
+    }
+  }
+
+  await uploadFile(url, '<p>Here is your new page. Make it good.</p>');
+
+  window.location.href = url;
 }
