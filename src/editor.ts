0510f75158cee8da18f1acc15723b7b7dde47cde
Oliver Zheng <gitcommits@oliverzheng.com>
2024-09-13 22:26:19 -0700

    Add linking in text

--- src/editor.ts
+++ src/editor.ts
@@ -1,12 +1,22 @@
 import invariant from 'invariant';
 import {HeadingNode, QuoteNode, registerRichText} from '@lexical/rich-text';
-import {$getRoot, $insertNodes, createEditor} from 'lexical';
+import {
+  $getRoot,
+  $insertNodes,
+  createEditor,
+  KEY_MODIFIER_COMMAND,
+  COMMAND_PRIORITY_NORMAL,
+  $getSelection,
+  $isRangeSelection,
+} from 'lexical';
 import {mergeRegister} from '@lexical/utils';
 import {$generateHtmlFromNodes, $generateNodesFromDOM} from '@lexical/html';
+import {LinkNode, $isLinkNode, AutoLinkNode, $toggleLink} from '@lexical/link';
+import {getSelectedNode} from './get-selected-node';
 
 const config = {
   namespace: 'MyEditor',
-  nodes: [HeadingNode, QuoteNode],
+  nodes: [HeadingNode, QuoteNode, LinkNode, AutoLinkNode],
   onError: console.error,
 };
 
@@ -32,6 +42,37 @@ export function turnOnEditMode() {
     $getRoot().select();
     $insertNodes(nodes);
   });
+
+  editor.registerCommand(
+    KEY_MODIFIER_COMMAND,
+    payload => {
+      const event: KeyboardEvent = payload;
+      const {code, ctrlKey, metaKey} = event;
+
+      if (code === 'KeyK' && (ctrlKey || metaKey)) {
+        event.preventDefault();
+
+        const selection = $getSelection();
+        if (!selection || !$isRangeSelection(selection)) {
+          return false;
+        }
+
+        const node = getSelectedNode(selection);
+        const parent = node.getParent();
+        const isLink = $isLinkNode(parent) || $isLinkNode(node);
+        let url: string | null;
+        if (!isLink) {
+          url = prompt('What url to link?', 'https://google.com/');
+        } else {
+          url = null;
+        }
+        $toggleLink(url);
+        return true;
+      }
+      return false;
+    },
+    COMMAND_PRIORITY_NORMAL,
+  );
 }
 
 export function turnOffEditMode() {
