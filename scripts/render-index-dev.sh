7dd03dfe213546102da76de8e181f47af1f86c62
Oliver Zheng <gitcommits@oliverzheng.com>
2024-09-21 17:01:43 -0700

    Make dev create a placeholder index if doesn't exist

---
+++ scripts/render-index-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ ! -f "stored-files/index.html" ]; then
+  echo "Creating placeholder index.html..."
+  sed -e 's/SCRIPT_PATH/\/src\/entry-client.ts/g' -e 's/<!--content-->/Edit me/g' "src/template.html" > "stored-files/index.html"
+fi
