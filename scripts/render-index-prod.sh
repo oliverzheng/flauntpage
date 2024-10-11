e813a0d4a0409c28731b1e27fda0d28dd7d695b3
Oliver Zheng <gitcommits@oliverzheng.com>
2024-10-10 21:01:35 -0700

    Update the css path

--- scripts/render-index-prod.sh
+++ scripts/render-index-prod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sed -e 's/SCRIPT_PATH/\/src\/entry-client.ts/g' -e 's/<!--content-->/Edit me/g' "src/template.html" > "index.html"
+sed \
+  -e 's/SCRIPT_PATH/\/src\/entry-client.ts/g' \
+  -e 's/STYLE_PATH/\/src\/style.css/g' \
+  -e 's/<!--content-->/Edit me/g' "src/template.html" \
+  > "index.html"
--- src/render-template.ts
+++ src/render-template.ts
@@ -4,7 +4,12 @@ export default function renderTemplate(
   contentHtml: string,
   entryScriptPath: string,
 ) {
+  const cssPath =
+    process.env.NODE_ENV === 'development'
+      ? '/src/style.css'
+      : '/assets/index.css';
   return template
     .replace(`<!--content-->`, contentHtml)
-    .replace('SCRIPT_PATH', entryScriptPath);
+    .replace('SCRIPT_PATH', entryScriptPath)
+    .replace('STYLE_PATH', cssPath);
 }
